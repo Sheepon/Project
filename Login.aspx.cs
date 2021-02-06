a256b58adb39963a6977dbfe4cafee88e75e84e2
FlamerKnight <69776403+FlamerKnight@users.noreply.github.com>
2021-02-07 00:13:08 +0800

    Add files via upload

---
+++ Login.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+using Project.App_Code;
+using System.Security.Cryptography;
+
+namespace Project
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        static int attemptCounts = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginButton_Click(object sender, EventArgs e)
+        {
+            attemptCounts = attemptCounts + 1;
+            SqlDataReader dR;
+            string strPassword;
+
+            DatabaseMgmt dbObj = new DatabaseMgmt();
+
+            string selStr = $"select passwd, name, ShopperId from SHopper Where email ='{emailTextBox.Text}'";
+
+            dR = dbObj.ExecuteSelect(selStr);
+            if (dR.Read())
+            {
+                if (attemptCounts < 3)
+                {
+                        strPassword = dR["passwd"].ToString();
+
+                        if (strPassword == pswTextBox.Text)
+                        {
+                            Session["Password"] = strPassword;
+                            Session["Name"] = dR["name"].ToString();
+                            Session["ShopperId"] = dR["ShopperID"].ToString();
+                            Response.Redirect("Home.aspx");
+                        }
+                        else
+                        {
+                            msgLabel.Text = "Email or password incorrect";
+                        }
+                    
+                }
+
+                else if (attemptCounts == 3)
+                {
+                    suggestionHyperLink.Visible = true;
+                    suggestionHyperLink.Text = "Forgot your password?";
+                    if (dR.Read())
+                    {
+                        strPassword = dR["passwd"].ToString();
+
+                        if (strPassword == pswTextBox.Text)
+                        {
+                            Session["Password"] = strPassword;
+                            Session["Name"] = dR["name"].ToString();
+                            Session["ShopperId"] = dR["ShopperID"].ToString();
+                            Response.Redirect("Home.aspx");
+                        }
+                        else
+                        {
+                            msgLabel.Text = "Email or password incorrect";
+                        }
+                    }
+                }
+
+                else
+                {
+                    if (attemptCounts == 3)
+                    {
+                        suggestionHyperLink.Visible = true;
+                        suggestionHyperLink.Text = "Forgot your password?";
+                    }
+                    else
+                    {
+                        //timeout
+                        msgLabel.Text = "Reload the Web";
+                        
+                    }
+                }
+       
+            }
+            else
+            {
+                msgLabel.Text = "Email or password incorrect";
+            }
+            dR.Close();
+        }
+
+        protected void resetButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ResetEmailorPassword.aspx");
+        }
+    }
+}
---
+++ Register.aspx.cs
@@ -0,0 +1,70 @@
+﻿using Project.App_Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RegisterButton_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid) {
+                object returnVal;
+                string strName, strAddress, strPhone, strPassword, strEmail, strQuestion, strAnswer;
+
+                int intShopperID = 0;
+
+                strName = nameTextBox.Text;
+                strAddress = addressTextbox.Text;
+                strPhone = phoneTextbox.Text;
+                strEmail = emailTextbox.Text;
+                strPassword = pswTextBox.Text.Trim();
+               strQuestion = DropDownList1.SelectedValue;
+                strAnswer = SecurityTextBox.Text;
+
+                DatabaseMgmt dbObj = new DatabaseMgmt();
+                
+                string strSqlCmd;
+                strSqlCmd = $"select ShopperID from Shopper where Email='{strEmail}'";
+
+                if ((returnVal = (object)dbObj.ExecuteScalar(strSqlCmd)) != null) {
+                    intShopperID = Convert.ToInt32(returnVal);
+                } else {
+                    intShopperID = 0;
+                }
+
+                if(intShopperID == 0) {
+                    strSqlCmd = $"Insert into Shopper(Name, Address,Phone, Email, Passwd, SecurityQn, QnAnswer) VALUES ('{strName}','{strAddress}'," +
+                        $"'{strPhone}','{strEmail}','{strPassword}','{strQuestion}','{strAnswer}')";
+                    dbObj.ExecuteNonQuery(strSqlCmd);
+
+                    strSqlCmd = $"select MAX(ShopperID) From Shopper";
+                    if ((returnVal = (object)dbObj.ExecuteScalar(strSqlCmd)) != null) {
+                        intShopperID = Convert.ToInt32(returnVal);
+                    } else {
+                        intShopperID = 0;
+                    }
+
+                    Session["ShopperID"] = intShopperID;
+                    Session["Name"] = strName;
+                    Session["Address"] = strAddress;
+                    Session["Phone"] = strPhone;
+                    Session["Email"] = strEmail;
+                    Session["ProfileRetrived"] = 1;
+                    msgLabel.Text = "Registration Successful";
+                } else {
+                    msgLabel.Text = "Please select another email to register";
+                }
+            } 
+        }
+    }
+}
